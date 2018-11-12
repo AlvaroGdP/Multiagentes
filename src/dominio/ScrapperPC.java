b228581d47b347ac6d5514bbabdf18b7ac235bb5
AlvaroGdP <alvaro.guerrero1997@gmail.com>
2018-11-12 19:42:43 +0100

    Renamed classes
    Now "Agente" refers to jade Agent implementation of Agents
    
    "Scrapper" classes are to be removed soon

--- src/dominio/ScrapperPC.java
+++ src/dominio/ScrapperPC.java
@@ -7,7 +7,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-public class AgentePC {
+public class ScrapperPC {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
